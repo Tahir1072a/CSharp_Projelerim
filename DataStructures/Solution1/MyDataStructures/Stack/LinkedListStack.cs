bfa7f3a961fee54282565b9abe07486b8d82a5b0
Tahir1072a <tahirifdn@gmail.com>
2023-07-10 13:34:38 +0300

    Stack Bitti.

---
+++ DataStructures/Solution1/MyDataStructures/Stack/LinkedListStack.cs
@@ -0,0 +1,27 @@
+﻿using MyDataStructures.LinkedList.MySinglyLinkedList;
+
+namespace MyDataStructures.Stack
+{
+    internal class LinkedListStack<T> : IStack<T>
+    {
+        public int Count { get; private set; }
+        private readonly SinglyLinkedList<T> linkedList = new();
+
+        public T Peek()
+        {
+            return linkedList.Head.Item;
+        }
+
+        public T Pop()
+        {
+            Count--;
+            return linkedList.RemoveFirst();
+        }
+
+        public void Push(T value)
+        {
+            linkedList.AddFirst(value);
+            Count++;
+        }
+    }
+}
