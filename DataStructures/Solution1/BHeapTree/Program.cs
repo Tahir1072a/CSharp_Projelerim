a78782e38b728e39e84a7d3e342a04d4924f5bfe
Tahir1072a <tahirifdn@gmail.com>
2023-07-31 13:38:38 +0300

    BHeap Bitti!

---
+++ DataStructures/Solution1/BHeapTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyDataStructures.Tree.BinaryTrees.BHeap;
+Random rnd = new Random();
+int[] ints = new int[30000000];
+
+
+var heap = new MinHeap<int>();
+heap.Add(4);
+heap.Add(1);
+heap.Add(10);
+heap.Add(8);
+heap.Add(7);
+heap.Add(5);
+heap.Add(9);
+heap.Add(3);
+heap.Add(2);
+heap.DeleteMinMax();
+
+
+var maxHeap = new MaxHeap<int>();
+maxHeap.Add(4);
+maxHeap.Add(1);
+maxHeap.Add(10);
+maxHeap.Add(8);
+maxHeap.Add(7);
+maxHeap.Add(5);
+maxHeap.Add(9);
+maxHeap.Add(3);
+maxHeap.Add(2);
+maxHeap.DeleteMinMax();
+Console.ReadLine();
---
+++ DataStructures/Solution1/MyDataStructures/Tree/BinaryTrees/BHeap/MinHeap.cs
@@ -0,0 +1,49 @@
+﻿namespace MyDataStructures.Tree.BinaryTrees.BHeap
+{
+    public class MinHeap<T> : BHeapTree<T>, IEnumerable<T> where T : IComparable
+    {
+        public MinHeap() : base()
+        {
+            
+        }
+        public MinHeap(int size) : base(size)
+        {
+            
+        }
+        public MinHeap(IEnumerable<T> collection) : base(collection)
+        {
+            
+        }
+        protected override void HeapifyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var smallerIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetLeftChild(index).CompareTo(GetRightChild(index)) > 0)
+                {
+                    smallerIndex = GetRightChildIndex(index);
+                }
+                if (Array[smallerIndex].CompareTo(Array[index]) >= 0) break;
+                Swap(smallerIndex, index);
+                index = smallerIndex;
+            }
+
+        }
+
+        protected override void HeapifyUp()
+        {
+            int index = position - 1;
+            while(index != 0)
+            {
+                int smallerIndex = GetParentIndex(index);
+                if (GetParent(index).CompareTo(Array[index]) <= 0)
+                {
+                    break;
+                }
+                Swap(smallerIndex, index);
+                index = smallerIndex;
+            }
+        }
+    }
+}
